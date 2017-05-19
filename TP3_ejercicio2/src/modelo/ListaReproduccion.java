a0c60cf34cbf9611f4c24cde8deff5c78664fa91
diegohunko <diegofernandezhunko@outlook.com.ar>
2017-05-18 23:00:11 -0300

    Modificado el método insertar(Cancion nuevaCancion) de la clase ListaReproduccion; incorporado el método redimensionar que asigna una nueva logitud al array listaReproduccion.

--- TP3_ejercicio2/src/modelo/ListaReproduccion.java
+++ TP3_ejercicio2/src/modelo/ListaReproduccion.java
@@ -20,6 +20,15 @@ public class ListaReproduccion {
         this.contadorCanciones = 0;
     }
     
+    private void redimensionar(){
+        Cancion[] otraLista;
+        if (this.contadorCanciones == this.listaReproduccion.length || this.listaReproduccion.length == 1){
+            otraLista = new Cancion[this.contadorCanciones + 1];
+            java.lang.System.arraycopy(this.listaReproduccion, this.contadorCanciones - 1, otraLista, this.contadorCanciones - 1, this.listaReproduccion.length);
+            this.listaReproduccion = otraLista;
+        }
+    }
+    
     public void insertar(Cancion nuevaCancion){
         /**Escriba un método insertar en la clase ListaReproduccion
          * que inserta la canción especificada como parámetro al final
@@ -27,7 +36,11 @@ public class ListaReproduccion {
          * 
          * public void insertar (Cancion nuevaCancion)
          */
-        listaReproduccion[listaReproduccion.length] = nuevaCancion;
+        
+        this.listaReproduccion[this.contadorCanciones] = nuevaCancion;
+         this.contadorCanciones += 1;
+        this.redimensionar();
+       
         
     }
     
@@ -56,7 +69,7 @@ public class ListaReproduccion {
     }
     
     public int getCantidadCanciones (){
-        
+        return 0;
     }
     
     public void removerEn (int indice){
